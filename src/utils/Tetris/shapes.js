aeb36ac7744785d9c5c061dd29b4a7462b6bad5f
zuzexx <tjasa.zilavec@gmail.com>
2022-11-05 10:20:03 +0100

    a bit of refracturing and movement

---
+++ src/utils/Tetris/shapes.js
@@ -0,0 +1,33 @@
+let width = 10;
+const lShape = [
+  [1, width + 1, width * 2 + 1, 2],
+  [width, width + 1, width + 2, width * 2 + 2],
+  [1, width + 1, width * 2 + 1, width * 2],
+  [width, width * 2, width * 2 + 1, width * 2 + 2],
+];
+const zShape = [
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+];
+const oShape = [
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+];
+const tShape = [
+  [1, width, width + 1, width + 2],
+  [1, width + 1, width + 2, width * 2 + 1],
+  [width, width + 1, width + 2, width * 2 + 1],
+  [1, width, width + 1, width * 2 + 1],
+];
+const iShape = [
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+];
+
+export { lShape, zShape, oShape, tShape, iShape };
--- src/utils/tetris.js
+++ src/utils/tetris.js
@@ -1,5 +1,7 @@
+import { lShape, zShape, oShape, tShape, iShape } from "./Tetris/shapes";
+
 document.addEventListener("DOMContentLoaded", () => {
-  const grid = document.querySelectorAll(".grid-element");
+  //const grid = document.querySelectorAll(".grid-element");
   let squares = Array.from(document.querySelectorAll(".grid-element div"));
   const width = 10;
 
@@ -7,42 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
   //const startButton = document.querySelector("#start-button");
 
   //* SHAPES
-  const lShape = [
-    [1, width + 1, width * 2 + 1, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2 + 1, width * 2 + 2],
-  ];
-  const zShape = [
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-  ];
-  const oShape = [
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-  ];
-  const tShape = [
-    [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2 + 1],
-    [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, width * 2 + 1],
-  ];
-  const iShape = [
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3],
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3],
-  ];
 
   const shapes = [lShape, zShape, oShape, tShape, iShape];
 
   //* RANDOMLY SELECT THE SHAPE AND STARTING POSITION
   //let randomInteger = Math.floor(Math.random() * shapes.length);
-  let startingPosition = Math.floor(Math.random() * 9);
+  let startingPosition = Math.floor(Math.random() * 8);
+  if (startingPosition === 0 || startingPosition === 1) {
+    startingPosition += 1;
+  }
   let randomShape = Math.floor(Math.random() * 5);
   let randomRotation = Math.floor(Math.random() * 4);
   let currentShape = shapes[randomShape][randomRotation];
@@ -76,7 +51,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("keyup", controls);
 
   //? make the shape move down every interval
-
   let moveDown = () => {
     undraw();
     startingPosition = startingPosition + width;
@@ -86,7 +60,6 @@ document.addEventListener("DOMContentLoaded", () => {
   let timer = setInterval(moveDown, 1000);
 
   //? write a function to stop the shape from going below the grid
-
   let stopShape = () => {
     if (
       currentShape.some((index) =>
@@ -100,34 +73,31 @@ document.addEventListener("DOMContentLoaded", () => {
       );
 
       //? after the current shape stops it is time for the new shape to start falling
-      /* let chooseShape = Math.floor(Math.random() * shapes.length);
-      currentShape = shapes[chooseShape][chooseShape];*/
-      /* randomInteger = Math.floor(Math.random() * shapes.length);
-      currentShape = shapes[randomInteger][randomInteger];
-      currentShape =
-        shapes[Math.floor(Math.random() * 5)][Math.floor(Math.random() * 4)];*/
-
       randomShape = Math.floor(Math.random() * 5);
       randomRotation = Math.floor(Math.random() * 4);
       currentShape = shapes[randomShape][randomRotation];
-      startingPosition = Math.floor(Math.random() * 9);
+      startingPosition = Math.floor(Math.random() * 8);
+      if (startingPosition === 0 || startingPosition === 1) {
+        startingPosition += 1;
+      }
       draw();
     }
   };
+
   let moveLeft = () => {
     undraw();
     const isAtLeftEdge = currentShape.some(
       (index) => (startingPosition + index) % width === 0
     );
     if (!isAtLeftEdge) {
-      startingPosition = startingPosition - 1;
+      startingPosition -= 1;
     }
     if (
       currentShape.some((index) =>
         squares[startingPosition + index].classList.contains("stop-shape")
       )
     ) {
-      startingPosition = startingPosition + 1;
+      startingPosition += 1;
     }
     draw();
   };
@@ -135,64 +105,20 @@ document.addEventListener("DOMContentLoaded", () => {
   let moveRight = () => {
     undraw();
     const isAtRightEdge = currentShape.some(
-      (index) => (startingPosition + index) % width === 0
+      (index) => (startingPosition + index) % width === width - 1
     );
     if (!isAtRightEdge) {
-      startingPosition = startingPosition + 1;
+      startingPosition += 1;
     }
     if (
       currentShape.some((index) =>
         squares[startingPosition + index].classList.contains("stop-shape")
       )
     ) {
-      startingPosition = startingPosition - 1;
+      startingPosition -= 1;
     }
     draw();
   };
-  /*
-  function isAtRight() {
-    return currentShape.some(
-      (index) => (startingPosition + index + 1) % width === 0
-    );
-  }
-
-  function isAtLeft() {
-    return currentShape.some(
-      (index) => (startingPosition + index) % width === 0
-    );
-  }
-
-  let checkPositionBeforeRotation = (position) => {
-    position = position || startingPosition;
-    if ((position + 1) % width < 4) {
-      if (isAtRight()) {
-        startingPosition = startingPosition + 1;
-        checkPositionBeforeRotation(position);
-      } else if (isAtLeft()) {
-        startingPosition = startingPosition - 1;
-        checkPositionBeforeRotation(position);
-      }
-    }
-  };
-
-  let rotateShape = () => {
-    undraw();
-
-    if (randomRotation < 3) {
-      randomRotation = randomRotation + 1;
-    } else {
-      randomRotation = 0;
-    }
-
-    currentShape = shapes[randomShape][randomRotation];
-    checkPositionBeforeRotation();
-    draw();
-  };*/
-
-  console.log(squares);
-  console.log(width);
-  console.log(grid);
-  console.log(shapes);
   console.log(startingPosition);
   console.log(timer);
 });
